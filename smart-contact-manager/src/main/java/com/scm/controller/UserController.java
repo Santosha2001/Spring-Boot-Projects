00866e0d57351276fb685b9fed7dbe1b61c24bee
Santosha C <santosha5856@gmail.com>
2024-06-04 10:05:12 +0530

    smart

--- smart-contact-manager/src/main/java/com/scm/controller/UserController.java
+++ smart-contact-manager/src/main/java/com/scm/controller/UserController.java
@@ -1,19 +1,12 @@
 package com.scm.controller;
 
-import java.security.Principal;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.scm.entities.User;
-import com.scm.helpers.Helper;
 import com.scm.services.UserService;
 
 @Controller
@@ -22,7 +15,7 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-    private Logger logger = LoggerFactory.getLogger(UserController.class);
+    // private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController() {
         super();
